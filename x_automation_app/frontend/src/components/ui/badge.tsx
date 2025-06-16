b90729f256cbd9855ad1d52a101381d66d069a1d
zedems01 <nguessie.achille@gmail.com>
2025-06-16 23:00:54 +0200

    errors fixed

--- x_automation_app/frontend/src/components/ui/badge.tsx
+++ x_automation_app/frontend/src/components/ui/badge.tsx
@@ -17,6 +17,10 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        green:
+          "border-transparent bg-green-500 text-white [a&]:hover:bg-green-500/80",
+        yellow:
+          "border-transparent bg-yellow-500 text-white [a&]:hover:bg-yellow-500/80",
       },
     },
     defaultVariants: {
