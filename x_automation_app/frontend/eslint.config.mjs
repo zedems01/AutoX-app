ee70ac539a70041080d63f91ff62bdd28552b678
zedems01 <nguessie.achille@gmail.com>
2025-10-12 21:56:02 +0200

    warning solved

--- x_automation_app/frontend/eslint.config.mjs
+++ x_automation_app/frontend/eslint.config.mjs
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginReact from "eslint-plugin-react";
+import nextPlugin from '@next/eslint-plugin-next';
 import { defineConfig } from "eslint/config";
 
 
@@ -11,6 +13,10 @@ export default defineConfig([
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    plugins: {
+      '@next/next': nextPlugin,
+      'react': pluginReact,
+    },
     rules: {
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
