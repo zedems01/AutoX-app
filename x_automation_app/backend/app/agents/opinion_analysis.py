b45ea8f08c4f938f5e37e6429317a2c3a8a10127
zedems01 <nguessie.achille@gmail.com>
2025-10-13 05:37:44 +0200

    ci set up

--- x_automation_app/backend/app/agents/opinion_analysis.py
+++ x_automation_app/backend/app/agents/opinion_analysis.py
@@ -53,10 +53,8 @@ def opinion_analysis_node(state: OverallState) -> Dict[str, Any]:
         if not tweets:
             raise ValueError("No tweets found in the state to analyze.")
 
-        # tweets_json_string = json.dumps([tweet.model_dump() for tweet in tweets])
         data = [tweet.model_dump() for tweet in tweets]
         csv_data = data_to_csv(data)
-        # prompt = opinion_analysis_prompt.format(tweets=tweets_json_string)
         prompt = opinion_analysis_prompt.format(tweets=csv_data)
         print(prompt)
 
@@ -69,15 +67,7 @@ def opinion_analysis_node(state: OverallState) -> Dict[str, Any]:
             "overall_sentiment": analysis_result.overall_sentiment,
             "topic_from_opinion_analysis": analysis_result.topic_from_opinion_analysis,
         }
-        # return {
-        #     "opinion_summary": "The tweets express significant concern and suspicion regarding Israel's potential actions to draw the United States into a war with Iran. There's a prevailing belief that this is a deliberate strategy, with some users even implicating figures like Donald Trump or Benjamin Netanyahu in a plot to force US military involvement. The conversation highlights anxieties about the US being manipulated into a conflict that is not in its best interest.",
-        #     "overall_sentiment": "negative",
-        #     "topic_from_opinion_analysis": "US potential involvement in an Israel-Iran conflict",
-        #     "has_user_provided_topic":False,
-        #     "is_autonomous_mode":True,
-        #     "output_destination": "GET_OUTPUTS",
-        # }
 
     except Exception as e:
         logger.error(f"An error occurred in the opinion analysis node: {e}\n")
-        return {"error_message": f"An unexpected error occurred during opinion analysis: {str(e)}"}
+        return {"error_message": f"An unexpected error occurred during opinion analysis: {str(e)}"} 
